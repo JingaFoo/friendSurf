ca209dc5d56e1bb806c7d3345bab63232ec2fd1a
JingaFoo <calvinmwariama@yahoo.com>
2018-11-23 18:33:51 -0500

    inital folder structure

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "plugins": [
+      "react"
+    ],
+    "extends": "airbnb",
+    "rules": {
+      "linebreak-style": 0,
+      "react/jsx-filename-extension": 0,
+      "class-methods-use-this": 0,
+      "import/first": 0,
+      "import/extensions": 0,
+      "import/no-named-as-default-member": 0,
+      "import/no-named-as-default": 0,
+      "react/prefer-stateless-function": 0,
+      "jsx-a11y/click-events-have-key-events": 0,
+      "jsx-a11y/no-static-element-interactions": 0,
+      "react/forbid-prop-types": 0,
+      "import/no-extraneous-dependencies": 0,
+      "no-underscore-dangle": 0,
+      "prefer-template": 0,
+      "jsx-a11y/label-has-for": 0,
+      "no-case-declarations": 0,
+      "react/no-did-mount-set-state": 0,
+      "for-direction": 0,
+      "no-plusplus": 0,
+      "camelcase": 0,
+    },
+    "env": {
+      "browser": true,
+      "node": true,
+      "es6": true
+    }
+};
